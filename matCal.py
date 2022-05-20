aead9da4c84c5edbb03df8038341810c5b4c8337
David <davvyap@gmail.com>
2022-05-20 11:54:07 +0800

    Streamline reading the report process and analysis

--- matCal.py
+++ matCal.py
@@ -6,166 +6,72 @@ concrete = []
 sand = []
 stone = []
 
-date1 = "16022022"
 
-# # to calculate the start date & number of days for the report:
-# date_start = input("Inout the start date for the 1st report:")
-# date_end = input("Input the end date for final report:")
-# days = date_start-date_end
+# to calculate the start date & number of days for the report:
+year=2022
+month = int(input("Input report month:"))
+date_start = int(input("Input the start date for the 1st report:"))
+date_end = int(input("Input the end date for final report:"))
+days = date_end-date_start
+# print(days)
 
-# def date_nums(days):
-#     for i in range(days):
-#         date.append(i)
-#     return date
+def date_nums(day):
+    for i in range(day+1):
+        date.append(date_start+i)
+    return date
+print("Selected report dates: ")
+print(date_nums(days))
 
+# date1 = date[0]
+# report1 = open('workreport-short_'+str(date1)+str(month)+str(year)+'.txt','r')
+# read1 = report1.readlines()
+# print(read1)
 
-report1 = open('C:/Users/tatwai/Documents/David/Python/Scripts/Exercise/Work done report/workreport-short_'+date1+'.txt')
 
 # function to find materials usage:
 def material_usage(material):
-    for line in report1:
+    for line in read:
         line = line.rstrip()
         # print(line)
         wds = line.split()
         # print(wds)
 
         if len(wds)>1 and wds[0] == material:
-            return wds[1]
-    # else:    
-    #     continue
-
-concrete_usage1 = int(material_usage("Concrete"))
-sand_usage1 = int(material_usage('Sand'))
-stone_usage1 = int(material_usage('Stone'))
-
-print('Date=',date1,concrete_usage1,sand_usage1,stone_usage1)
-# print(type(concrete_usage1))
-date.append(date1)
-concrete.append(concrete_usage1)
-sand.append(sand_usage1)
-stone.append(stone_usage1)
+            return wds[-2]
 
 
-date2 = 17
-report2 = open('C:/Users/tatwai/Documents/David/Python/Scripts/Exercise/Work done report/workreport-short_17022022.txt')
+for i in range(len(date)):
+    print(i,end= " ") 
+    report = open('workreport-short_'+str(date[i])+str(month)+str(year)+'.txt','r')
+    read = report.readlines()
+    concrete_usage = int(material_usage('Concrete'))
+    concrete.append(concrete_usage)
+    sand_usage = int(material_usage('Sand'))
+    sand.append(sand_usage)
+    stone_usage = int(material_usage('Stone'))
+    stone.append(stone_usage)
 
-# function to find materials usage:
-def material_usage(material):
-    for line in report2:
-        line = line.rstrip()
-        # print(line)
-        wds = line.split()
-        # print(wds)
-
-        if len(wds)>1 and wds[0] == material:
-            return wds[1]
-    # else:    
-    #     continue
-
-concrete_usage2 = int(material_usage("Concrete"))
-sand_usage2 = int(material_usage('Sand'))
-stone_usage2 = int(material_usage('Stone'))
-
-print('Date=',date2,concrete_usage2,sand_usage2,stone_usage2)
-date.append(date2)
-concrete.append(concrete_usage2)
-sand.append(sand_usage2)
-stone.append(stone_usage2)
+print('\nStone usage list:')
+print(concrete)
+print('Sand usage list:')
+print(sand)
+print('Stone usage list:')
+print(stone)
 
 
-date3 = 18
-report3 = open('C:/Users/tatwai/Documents/David/Python/Scripts/Exercise/Work done report/workreport-short_18022022.txt')
+plt.xlabel("Date")
 
-# function to find materials usage:
-def material_usage(material):
-    for line in report3:
-        line = line.rstrip()
-        # print(line)
-        wds = line.split()
-        # print(wds)
-
-        if len(wds)>1 and wds[0] == material:
-            return wds[1]
-    # else:    
-    #     continue
-
-concrete_usage3 = int(material_usage("Concrete"))
-sand_usage3 = int(material_usage('Sand'))
-stone_usage3 = int(material_usage('Stone'))
-
-print('Date=',date3,concrete_usage3,sand_usage3,stone_usage3)
-date.append(date3)
-concrete.append(concrete_usage3)
-sand.append(sand_usage3)
-stone.append(stone_usage3)
-
-
-date4 = 19
-report4 = open('C:/Users/tatwai/Documents/David/Python/Scripts/Exercise/Work done report/workreport-short_19022022.txt')
-
-# function to find materials usage:
-def material_usage(material):
-    for line in report4:
-        line = line.rstrip()
-        # print(line)
-        wds = line.split()
-        # print(wds)
-
-        if len(wds)>1 and wds[0] == material:
-            return wds[1]
-    # else:    
-    #     continue
-
-concrete_usage4 = int(material_usage("Concrete"))
-sand_usage4 = int(material_usage('Sand'))
-stone_usage4 = int(material_usage('Stone'))
-
-print('Date=',date4,concrete_usage4,sand_usage4,stone_usage4)
-date.append(date4)
-concrete.append(concrete_usage4)
-sand.append(sand_usage4)
-stone.append(stone_usage4)
-
-
-date5 = 20
-report5 = open('C:/Users/tatwai/Documents/David/Python/Scripts/Exercise/Work done report/workreport-short_20022022.txt')
-
-# function to find materials usage:
-def material_usage(material):
-    for line in report5:
-        line = line.rstrip()
-        # print(line)
-        wds = line.split()
-        # print(wds)
-
-        if len(wds)>1 and wds[0] == material:
-            return wds[1]
-    # else:    
-    #     continue
-
-concrete_usage5 = int(material_usage("Concrete"))
-sand_usage5 = int(material_usage('Sand'))
-stone_usage5 = int(material_usage('Stone'))
-
-print('Date=',date5,concrete_usage5,sand_usage5,stone_usage5)
-date.append(date5)
-concrete.append(concrete_usage5)
-sand.append(sand_usage5)
-stone.append(stone_usage5)
-
-
-plt.xlabel("date")
 # to show concrete usage in a week:
-# plt.ylabel("concrete")
-# plt.plot(date,concrete)
-# plt.show()
+plt.ylabel("Concrete usage")
+plt.plot(date,concrete)
+plt.show()
 
 # # sand 
-plt.ylabel("sand")
+plt.ylabel("Sand usage")
 plt.plot(date,sand)
 plt.show()
 
 # # stone
-# plt.ylabel("stone")
-# plt.plot(date,stone)
-# plt.show()
+plt.ylabel("Stone usage")
+plt.plot(date,stone)
+plt.show()
